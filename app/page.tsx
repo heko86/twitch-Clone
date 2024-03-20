fedaf5a301549227e52f2d891c2930ccbceede66
heko86 <ryou86pg@gmail.com>
2024-03-21 02:21:04 +0900

    認証機能の追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-  <Button>Click me</Button>;
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
